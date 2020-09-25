367ea661ef142c3c1f009bcacd29ea97d6a07e5a
Ladarius Brooks <lbrooks2312@yahoo.com>
2020-09-25 13:03:04 -0700

    108 - competency report

---
+++ src/components/item.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import QuantityPicker from './quantityPicker';
+import "./item.css";
+
+class Item extends Component {
+    state = {
+        minimum: this.props.product.minimum || 1,
+        quantity: this.props.product.minimum || 1,
+     };
+    render() { 
+        return ( 
+            <div className="item">
+                <img src={"/images/products/" + this.props.product.image} alt="Product"></img>
+
+                <h4>{this.props.product.title}</h4>
+
+                <div className="prices">
+                    <h5>${this.props.product.price * this.state.quantity}</h5>
+                    <h6>Price ${this.props.product.price}</h6>
+                </div>
+
+                <QuantityPicker minimum={this.state.minimum} value={this.state.quantity} onValueChange={ (qnt) => this.handleQuantityChange(qnt) }></QuantityPicker>
+
+                <button className= "btn-add btn btn-sm btn-primary">Add</button>
+            </div>
+         );
+    }
+
+    handleQuantityChange = (qnt) => {
+        this.setState({ quantity: qnt});
+    };
+}
+ 
+export default Item;
